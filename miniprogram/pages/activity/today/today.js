2bfe8502683f129f16871ebd8fbf576473d50226
xiejx <15975129956@139.com>
2020-01-20 15:13:22 +0800

    完成今日待办页面最初版本

--- miniprogram/pages/activity/today/today.js
+++ miniprogram/pages/activity/today/today.js
@@ -5,8 +5,15 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    todoTask: [],  // 待办任务
+    fullfilTask: [],  // 已完成任务
+    inputTask: null,  // 用于添加任务之后清空输入框
   },
+
+  /**
+   * 其他数据 
+   */
+  inputData: null,  // 输入数据
 
   /**
    * 生命周期函数--监听页面加载
@@ -62,5 +69,61 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+
+  /**
+   * 添加任务
+   */
+  addTask: function (event) {
+    if (this.inputData != null && this.inputData.trim() != null) {
+      this.data.todoTask.push(this.inputData);
+      this.setData({
+        todoTask: this.data.todoTask,
+        inputTask: null,
+      });
+    }
+  },
+
+  /**
+   * 输入今日待办内容
+   */
+  inputTask: function (event) {
+    this.inputData = event.detail.value;
+  },
+
+  /**
+   * 删除任务
+   */
+  deleteTask: function (event) {
+    let taskId = event.currentTarget.dataset.taskid;
+    console.log(taskId);
+    this.data.todoTask.splice(taskId, 1);
+    this.setData({
+      todoTask: this.data.todoTask,
+    });
+  },
+
+  /**
+   * 完成任务
+   */
+  compTask: function (event) {
+    let taskId = event.currentTarget.dataset.taskid;
+    var temp = this.data.todoTask.splice(taskId, 1);
+    this.setData({
+      todoTask: this.data.todoTask,
+      fullfilTask: this.data.fullfilTask.concat(temp),
+    });
+  },
+
+  /**
+   * 未完成任务
+   */
+  uncompTask: function (event) {
+    let taskId = event.currentTarget.dataset.taskid;
+    var temp = this.data.fullfilTask.splice(taskId, 1);
+    this.setData({
+      todoTask: this.data.todoTask.concat(temp),
+      fullfilTask: this.data.fullfilTask,
+    });
   }
 })
